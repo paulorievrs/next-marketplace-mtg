43a1d0c63a5075f6fc876de086fc3ed7e6074272
paulorievrs <paulorievrsoliveira@gmail.com>
2023-01-04 14:22:55 -0300

    refactor: changing footer style

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -5,12 +5,10 @@ export default function Footer() {
   return (
     <div className="lg:fixed lg:bottom-0 w-full">
       <div className="flex flex-col items-center pt-14 pb-16 gap-8 bg-dark-primary mt-24 md:flex-row md:justify-center md:gap-40">
-        <Body fontWeight="font-black" fontSize="text-base" color="text-white">
-          Calabouço do Android
+        <Body fontWeight="font-medium" fontSize="text-base" color="text-white">
+          Calabouço do Android - {new Date().getFullYear()}
         </Body>
-        <Body fontWeight="font-bold" fontSize="text-sm" color="text-white">
-          +7 7172 264 55 55
-        </Body>
+
         <div className="w-[130px]">
           <Button variant="outline">Заказать звонок</Button>
         </div>
