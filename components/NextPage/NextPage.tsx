3df8b8daf00e60b19926edcdc6bd3eeceb0649d5
paulorievrs <paulorievrsoliveira@gmail.com>
2023-01-04 14:04:03 -0300

    fix: change color variable name

--- components/NextPage/NextPage.tsx
+++ components/NextPage/NextPage.tsx
@@ -9,7 +9,7 @@ type NextPageProps = {
 export default function NextPage({ pageTitle }: NextPageProps) {
   return (
     <div className="flex flex-row justify-between items-center cursor-pointer w-full mt-8">
-      <Body fontWeight="font-bold" color="text-info-light" fontSize="text-lg">
+      <Body fontWeight="font-bold" color="text-light-info" fontSize="text-lg">
         {pageTitle}
       </Body>
       <ArrowRight />
